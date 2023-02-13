a61a8581aaaddb0e46cfe4d4b9b2940c46518b5c
Vanshika Raj Sharma <51854053+rsvanshika@users.noreply.github.com>
2023-02-13 22:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+var btn=document.getElement("btn");
+var numPtag= document.getElementById("num");
+
+function generateRandom( ) {
+	var no =Math.floor(Math.random()*40)-20;
+	numPtag.textcontent=no;
+}	
+	btn.addEventListener("click",generateRandom);
 
