21c6d2a7ef5af1605bb5927e33faecda37d3f369
Vanshika Raj Sharma <51854053+rsvanshika@users.noreply.github.com>
2023-02-17 21:42:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 var randomNum;
 var input;
 function randomNumGenerator(){
+	 //generate random number
 	randomNum = Math.floor(Math.random()*41) - 20;
+	 //show random number
 	document.getElementById("num").innerText = randomNum;
 	
+	//convert input into number
 	input = parseInt(document.getElementById("guess").value);	
 	
-
+    //check differnce and print
 	if(Math.abs(randomNum - input) <= 5){
 		document.getElementById("respond").innerText = "Hot";
 	}
