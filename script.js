f8efb333b122d4ea6fcd3d315a5cd6782f64c168
Vanshika Raj Sharma <51854053+rsvanshika@users.noreply.github.com>
2023-02-17 21:12:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 //your code here
-var btn=document.getElement("btn");
+var btn=document.getElementById("btn");
+var inputVal=document.getElementById("guess").value ;
 var numPtag= document.getElementById("num");
+var res=document.getElementById("respond");
+
+
 
 function generateRandom( ) {
-	var no =Math.floor(Math.random()*40)-20;
-	numPtag.textcontent=no;
+	
+	var no =Math.floor(Math.random() *40)- 20;
+	numPtag.textContent=no;
+	     if(Math.abs(no-inputVal)<=5){ 
+			 res.textContent ="Hot";
+		 }
+	else{
+		res.textContent="cold";
+	}
+			 
+		 
 }	
-	btn.addEventListener("click",generateRandom);
+
+	
+
+	
+	btn.addEventListener("click",generateRandom());
 
