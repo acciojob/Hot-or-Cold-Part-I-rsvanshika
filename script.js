19a9db06502b425ef39e32958055e3666ff9e830
Vanshika Raj Sharma <51854053+rsvanshika@users.noreply.github.com>
2023-02-17 21:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var input;
 function randomNumGenerator(){
 	randomNum = Math.floor(Math.random()*41) - 20;
 	document.getElementById("num").innerHTML = randomNum;
-	guessName();
+	
+	input = parseInt(document.getElementById("guess").value);	
 	print();
 }
-function guessName() {
-	input = parseInt(document.getElementById("guess").value);	
-}
+
+
 function print(){
 	if(Math.abs(randomNum - input) <= 5){
 		document.getElementById("respond").innerHTML = "Hot";
